30c8d789fea3fd862fef48d92d89ead17a5cf969
siddiquenaim <naim.niter.tex06@gmail.com>
2024-01-02 13:42:48 +0600

    Completed basic table setup

--- app/components/BasicTable/BasicTable.jsx
+++ app/components/BasicTable/BasicTable.jsx
@@ -1,12 +1,23 @@
 "use client";
 
-import { flexRender, useReactTable } from "@tanstack/react-table";
+import {
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 import { columnDef } from "../columns";
 import "./BasicTable.css";
+import dataJSON from "../../data/data.json";
+import { useMemo } from "react";
 
 const BasicTable = () => {
+  const finalData = useMemo(() => dataJSON, []);
+  const finalColumnDef = useMemo(() => columnDef, []);
+
   const tableInstance = useReactTable({
-    columns: columnDef, // react table uses this to understand the headers
+    columns: finalColumnDef, // react table uses this to understand the headers
+    data: finalData, // this is the entire data
+    getCoreRowModel: getCoreRowModel(), // to have access to all the rows
   });
 
   return (
@@ -26,6 +37,38 @@ const BasicTable = () => {
             </tr>
           ))}
         </thead>
+        <tbody>
+          {tableInstance.getCoreRowModel().rows.map((rowEl) => {
+            return (
+              <tr key={rowEl.id}>
+                {rowEl.getVisibleCells().map((cellEl) => {
+                  return (
+                    <td key={cellEl.id}>
+                      {flexRender(
+                        cellEl.column.columnDef.cell,
+                        cellEl.getContext()
+                      )}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+        <tfoot>
+          {tableInstance.getHeaderGroups().map((headerEl) => (
+            <tr key={headerEl?.id}>
+              {headerEl.headers.map((columnEl) => (
+                <th key={columnEl?.id}>
+                  {flexRender(
+                    columnEl.column.columnDef.header, //this is where the headings renders
+                    columnEl.getContext()
+                  )}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </tfoot>
       </table>
     </div>
   );
--- app/components/columns.js
+++ app/components/columns.js
@@ -1,15 +1,18 @@
+import { createColumnHelper } from "@tanstack/react-table";
+
+const columnHelper = createColumnHelper();
+
 export const columnDef = [
-  {
-    accessorKey: "id",
+  columnHelper.accessor("id", {
     header: "ID",
-  },
+  }), //way-2: this is another way of defining columns
   {
     accessorKey: "first_name",
-    header: "First Name",
+    header: "First Name", //way-1: simplest way to define columns
   },
   {
-    accessorKey: "last_name",
-    header: "Last Name",
+    accessorFn: (row) => `${row.last_name}`,
+    header: "Last Name", //way-3: third way of doing it using function, cells can be merged this way `${row.last_name} ${row.first_name}`
   },
   {
     accessorKey: "email",
